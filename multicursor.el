621c09b25dd4bfdb26e38e755a6383b9d6bef44f
Eric Jones <eric.r.k.jones@gmail.com>
2019-05-02 09:46:01 -0600

    Added comments to multicursor.el describing desired changes.

--- multicursor.el
+++ multicursor.el
@@ -1,5 +1,14 @@
 (require 'multiple-cursors)
 
+;; There might be a way to fix the off-screen cursor issue that frustrates
+;; multiple-cursor mode.  If you were to replace the main cursor/point with
+;; a virtual cursor and place the main cursor in a jailed buffer or something
+;; so that its input didn't affect the open buffer, you could keep the
+;; functionality of multiple-cursors where the virtual cursors can be scrolled
+;; past the edges of the window.
+
+;; Wouldn't it make more sense for that to be a function of the main cursor, though?
+
 ;; Sublime-like edit each line of marked region with separate cursor
 (global-set-key (kbd "C-S-l") 'mc/edit-lines)
 
